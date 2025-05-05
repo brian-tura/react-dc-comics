c15f8f695b75ef498b208e37e8ef922543c20b32
brian-tura <b.tura2002@gmail.com>
2025-05-05 16:28:35 +0200

    [FEAT] Base Header structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <figure>
+            <img src="../src/assets/img/dc-logo.png" alt="dc-logo" />
+        </figure>
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </header>        
+  )
+}
+
+export default Header
