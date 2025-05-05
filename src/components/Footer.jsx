00cb872125ca80caa0ccba3e0dbc7f2216efe96d
brian-tura <b.tura2002@gmail.com>
2025-05-05 18:23:00 +0200

    [FEAT] Upper Footer part done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,22 +5,58 @@ const Footer = () => {
         <footer>
             <div className='first-footer'>
                 <div className="container">
-                        <ul>
-                            <li><h2>DC COMICS</h2></li>
-                            <li>Characters</li>
-                            <li>Comics</li>
-                            <li>Movies</li>
-                            <li>Tv</li>
-                            <li>Games</li>
-                            <li>Videos</li>
-                            <li>News</li>
-                        </ul>
-                        <ul></ul>
-                        <ul></ul>
-                        <ul></ul>
+                    <div className='content'>
+                        <div className='d-flex'>
+                            <div>
+                                <ul>
+                                    <li><h2>DC COMICS</h2></li>
+                                    <li>Characters</li>
+                                    <li>Comics</li>
+                                    <li>Movies</li>
+                                    <li>Tv</li>
+                                    <li>Games</li>
+                                    <li>Videos</li>
+                                    <li>News</li>
+                                </ul>
+                                <ul>
+                                    <li><h2>SHOP</h2></li>
+                                    <li>Shop DC</li>
+                                    <li>Shop DC Collectibles</li>
+                                </ul>
+                            </div>
+                            <ul>
+                                <li><h2>DC</h2></li>
+                                <li>Terms Of Use</li>
+                                <li>Privacy policy &#40;New&#41;</li>
+                                <li>Ad Choices</li>
+                                <li>Advertising</li>
+                                <li>Jobs</li>
+                                <li>Subscriptions</li>
+                                <li>Talent Workshops</li>
+                                <li>CPSC Certificates</li>
+                                <li>Ratings</li>
+                                <li>Shop Help</li>
+                                <li>Contact Us</li>
+                            </ul>
+                            <ul>
+                                <li><h2>SITES</h2></li>
+                                <li>DC</li>
+                                <li>MAD Magazine</li>
+                                <li>DC Kids</li>
+                                <li>DC Universe</li>
+                                <li>DC Power Visa</li>
+                            </ul>
+                        </div>
+                        <figure>
+                            <img src="../src/assets/img/dc-logo-bg.png" alt="" />
+                        </figure>
+                    </div>
                 </div>
             </div>
-        </footer>
+            <div className='second-footer'>
+
+            </div>
+        </footer >
     )
 }
 
