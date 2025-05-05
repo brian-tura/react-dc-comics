8a9e0b010543f0b5c16b114015e3daa11c07f44e
brian-tura <b.tura2002@gmail.com>
2025-05-05 17:22:22 +0200

    [FEAT] Main Done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,9 +3,24 @@ import React from 'react'
 const Main = () => {
     return (
         <main>
-            <div className='container'>
-                <div className='content-goes-here'>
-                    <h2>Content goes here</h2>
+            <div className='big-container'>
+                <div className='container'>
+                    <div className='content-goes-here'>
+                        <h2>Content goes here</h2>
+                    </div>
+                </div>
+            </div>
+            <div className='big-container-img'>
+                <div className='container'>
+                    <div className='content-goes-here'>
+                        <ul className='img-list'>
+                            <li><img src="../src/assets/img/buy-comics-digital-comics.png" alt="" /> DIGITAL COMICS</li>
+                            <li><img src="../src/assets/img/buy-comics-merchandise.png" alt="" /> DC MERCHANDISE</li>
+                            <li><img src="../src/assets/img/buy-comics-subscriptions.png" alt="" /> SUBSCRIPTION</li>
+                            <li><img src="../src/assets/img/buy-comics-shop-locator.png" alt="" /> COMICS SHOP LOCATOR</li>
+                            <li><img src="../src/assets/img/buy-dc-power-visa.svg" alt="" /> CD POWER VISA</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </main>
