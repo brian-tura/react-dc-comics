bd204923248a879ced376a911417410a97c81c28
brian-tura <b.tura2002@gmail.com>
2025-05-05 17:56:40 +0200

    [FEAT] Structure of footer upper part done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='first-footer'>
+                <div className="container">
+                        <ul>
+                            <li><h2>DC COMICS</h2></li>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>Tv</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+                        <ul></ul>
+                        <ul></ul>
+                        <ul></ul>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
