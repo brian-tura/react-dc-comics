6cb6ca02dcd24b2e2217d674f5082b5f1ead5976
brian-tura <b.tura2002@gmail.com>
2025-05-05 22:35:14 +0200

    [FIX] List now have hyperlink tag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,41 +10,41 @@ const Footer = () => {
                             <div>
                                 <ul>
                                     <li><h2>DC COMICS</h2></li>
-                                    <li>Characters</li>
-                                    <li>Comics</li>
-                                    <li>Movies</li>
-                                    <li>Tv</li>
-                                    <li>Games</li>
-                                    <li>Videos</li>
-                                    <li>News</li>
+                                    <li><a href="#">Characters</a></li>
+                                    <li><a href="#">Comics</a></li>
+                                    <li><a href="#">Movies</a></li>
+                                    <li><a href="#">Tv</a></li>
+                                    <li><a href="#">Games</a></li>
+                                    <li><a href="#">Videos</a></li>
+                                    <li><a href="#">News</a></li>
                                 </ul>
                                 <ul>
                                     <li><h2>SHOP</h2></li>
-                                    <li>Shop DC</li>
-                                    <li>Shop DC Collectibles</li>
+                                    <li><a href="#">Shop DC</a></li>
+                                    <li><a href="#">Shop DC Collectibles</a></li>
                                 </ul>
                             </div>
                             <ul>
                                 <li><h2>DC</h2></li>
-                                <li>Terms Of Use</li>
-                                <li>Privacy policy &#40;New&#41;</li>
-                                <li>Ad Choices</li>
-                                <li>Advertising</li>
-                                <li>Jobs</li>
-                                <li>Subscriptions</li>
-                                <li>Talent Workshops</li>
-                                <li>CPSC Certificates</li>
-                                <li>Ratings</li>
-                                <li>Shop Help</li>
-                                <li>Contact Us</li>
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy policy &#40;New&#41;</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contact Us</a></li>
                             </ul>
                             <ul>
                                 <li><h2>SITES</h2></li>
-                                <li>DC</li>
-                                <li>MAD Magazine</li>
-                                <li>DC Kids</li>
-                                <li>DC Universe</li>
-                                <li>DC Power Visa</li>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
                             </ul>
                         </div>
                         <figure>
