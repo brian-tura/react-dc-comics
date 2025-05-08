76f146304aee48dad09b259133c7addfaf56fe6a
brian-tura <b.tura2002@gmail.com>
2025-05-08 10:42:33 +0200

    [DONE] Excercise done, added cycle trhough comics object array

--- src/components/ListComics.jsx
+++ src/components/ListComics.jsx
@@ -4,21 +4,9 @@ import ComicCard from "./ComicCard";
 
 const ListComics = () => {
     return (
-        <>
-            <ComicCard series={comics[0].series} thumb={comics[0].thumb} />
-            <ComicCard series={comics[1].series} thumb={comics[1].thumb} />
-            <ComicCard series={comics[2].series} thumb={comics[2].thumb} />
-            <ComicCard series={comics[3].series} thumb={comics[3].thumb} />
-            <ComicCard series={comics[4].series} thumb={comics[4].thumb} />
-            <ComicCard series={comics[5].series} thumb={comics[5].thumb} />
-            <ComicCard series={comics[6].series} thumb={comics[6].thumb} />
-            <ComicCard series={comics[7].series} thumb={comics[7].thumb} />
-            <ComicCard series={comics[8].series} thumb={comics[8].thumb} />
-            <ComicCard series={comics[9].series} thumb={comics[9].thumb} />
-            <ComicCard series={comics[10].series} thumb={comics[10].thumb} />
-            <ComicCard series={comics[11].series} thumb={comics[11].thumb} />
-
-        </>
+        comics.map((comic) => (
+            <ComicCard series={comic.series} thumb={comic.thumb} />
+        ))
     )
 }
 
